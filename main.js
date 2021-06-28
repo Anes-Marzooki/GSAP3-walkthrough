0de6a5b66499d321bb325e1ac94d5ab78e049ed3
Anes-Marzooki <marzoukianas@gmail.com>
2021-06-28 20:59:39 +0100

    animate button

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-gsap.to('.tmux', {
-	rotationY: 180,
-	x: 400,
-	y: 200,
-	scale: 3,
-	duration: 3,
+const img = document.querySelector('.tmux');
+const btn = document.querySelector('.run');
+
+const animate = () => {
+	gsap.to(img, {
+		rotationY: 180,
+		x: 400,
+		y: 200,
+		scale: 3,
+		duration: 3,
+	});
+};
+
+window.addEventListener('load', () => {
+	btn.addEventListener('click', animate);
 });
